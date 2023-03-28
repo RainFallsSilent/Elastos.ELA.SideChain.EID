fcebe1e79c2939fd8646515a4327094dce9f4fc9
jiangzehua <1092431698@qq.com>
2023-03-28 18:07:53 +0800

    remove gorutine of PostChainEvents

--- core/blockchain.go
+++ core/blockchain.go
@@ -1526,7 +1526,7 @@ func (bc *BlockChain) InsertChain(chain types.Blocks) (int, error) {
 	bc.wg.Done()
 	log.Info("##@ Synchronising processFullSyncContent importBlockResults InsertChain 4")
 
-	go bc.PostChainEvents(events, logs)
+	bc.PostChainEvents(events, logs)
 	log.Info("##@ Synchronising processFullSyncContent importBlockResults InsertChain 5")
 
 	return n, err
